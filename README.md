3bab8ef761a73629ba9002eef6f1e02647370735
Donald Hokanson <donaldhokanson@gmail.com>
2016-06-28 17:02:22 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# One Month jQuery - Project #3
+
+This is the third project for the One Month jQuery class that focused on using local storage to persist data, DOM traversal, if/else statements, and variables.
+
+I have attached a file called 'project-3-template' so you can see where the project started.
---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+var main=function(){
+	
+	$('#list-items').html(localStorage.getItem('listItems'));
+
+	$('.add-items').submit(function(event){
+		event.preventDefault();
+		
+		var item = $('#todo-list-item').val();
+
+		if (item) {
+			$('#list-items').append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>X</a><hr></li>");
+			localStorage.setItem('listItems', $('#list-items').html());
+			$('#todo-list-item').val("");		
+		}
+	});
+};
+
+$(document).ready(main)
+
+$(document).on('change', '.checkbox', function(){
+	if ($(this).attr('checked')) {
+		$(this).removeAttr('checked');
+	}
+
+	else{
+		$(this).attr('checked', 'checked');
+	}
+
+	$(this).parent().toggleClass('completed');
+	localStorage.setItem('listItems', $('#list-items').html());
+});
+
+$(document).on('click', '.remove', function(){
+	$(this).parent().fadeOut('slow', function(){
+		$(this).remove();
+		localStorage.setItem('listItems', $('#list-items').html());
+	});
+});
